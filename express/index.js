5067f6a2d947f07a392d96722bde68744e935e35
Dilip Prajapati <dilipp11082004@gmail.com>
2024-10-25 15:33:13 +0530

    c

--- express/index.js
+++ express/index.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const PORT = 3000;
+app.use(express.json());
+let data = {
+  "1a": `
+# Graph definition
+graph = {
+  'A': ['B', 'C'],
+  'B': ['A', 'D', 'E'],
+  'C': ['A', 'F'],
+  'D': ['B'],
+  'E': ['B', 'F'],
+  'F': ['C', 'E']
+}
 
+# BFS function
+def bfs(start):
+  queue = [start]       # Start with the initial node
+  visited = []          # List to track visited nodes
+
+  while queue:
+      node = queue.pop(0)   # Remove the first node from the queue
+      if node not in visited:
+          visited.append(node)  # Mark the node as visited
+
+          # Add neighbors to the queue
+          for neighbor in graph[node]:
+              if neighbor not in visited:
+                  queue.append(neighbor)
+
+  return visited   # Return the list of visited nodes
+
+# Run BFS starting from 'A'
+print(bfs('A'))`,
+};
+
+// Route that returns JSON data
 app.get("/", (req, res) => {
-  fs.readFile("example.txt", "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send("Error reading code file");
-    }
-    res.setHeader("Content-Type", "text/plain");
-    res.send(data);
-  });
+  res.json();
 });
 
 app.listen(PORT, () => {
