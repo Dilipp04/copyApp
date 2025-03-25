4924f6022c8a7cdf15b208547d92d0bd70ae4514
Dilip Prajapati <dilipp11082004@gmail.com>
2025-03-25 23:25:29 +0530

    formated

--- express/index.js
+++ express/index.js
@@ -7,58 +7,57 @@ let data = {
   1: ``,
   2: ``,
   3: `
-> rainfall<-c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
-> rainfall.timeseries <- ts(rainfall,start = c(2012,1),frequency = 12)
-> print(rainfall.timeseries)
-> png(file = "rainfall.png")
-> plot(rainfall.timeseries)
-> dev.off()
+rainfall<-c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
+rainfall.timeseries <- ts(rainfall,start = c(2012,1),frequency = 12)
+print(rainfall.timeseries)
+png(file = "rainfall.png")
+plot(rainfall.timeseries)
+dev.off()
   
   `,
   4: `
-  > newiris <- iris
-> newiris$Species <- NULL
-> kc <- kmeans(newiris, 3)
-> table(iris$Species, kc$cluster)
-> plot(newiris[c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
-> points(kc$centers[, c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex = 2)
+newiris <- iris
+newiris$Species <- NULL
+kc <- kmeans(newiris, 3)
+table(iris$Species, kc$cluster)
+plot(newiris[c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
+points(kc$centers[, c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex = 2)
   `,
   5: `
-  > x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
-> y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
-> # Apply the lm() function.
-> relation <- lm(y~x)
-> print(relation)
-> print(summary(relation))
-> a <- data.frame(x = 170)
-> result <- predict(relation,a)
-> print(result)
-> png(file = "linearregression.png")
-> plot(y,x,col = "blue",main = "Height & Weight Regression",+ abline(lm(x~y)),cex =
-1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in + cm")
-> dev.off()
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+# Apply the lm() function.
+relation <- lm(y~x)
+print(relation)
+print(summary(relation))
+a <- data.frame(x = 170)
+result <- predict(relation,a)
+print(result)
+png(file = "linearregression.png")
+plot(y,x,col = "blue",main = "Height & Weight Regression",+ abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",ylab = "Height in + cm")
+dev.off()
   
   `,
   6: `
-  > x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
-> y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
-> y_binary <- ifelse(y > 60, 1, 0)
-> data <- data.frame(x = x, y = y, y_binary = y_binary)
-> logistic_model <- glm(y_binary ~ x, data = data, family = binomial)
-> summary(logistic_model)
-> predicted_prob <- predict(logistic_model, type = "response")
-> predicted_class <- ifelse(predicted_prob > 0.5, 1, 0)
-> table(Predicted = predicted_class, Actual = y_binary)
-> library(ggplot2)
-> ggplot(data, aes(x = x, y = y_binary)) +
-+ geom_point() +
-+ stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE) +
-+ labs(title = "Logistic Regression: Binary Outcome by x",
-+ x = "x",
-+ y = "Binary Outcome")
+install.packages("ggplot2")
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+y <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+y_binary <- ifelse(y > 60, 1, 0)
+data <- data.frame(x = x, y = y, y_binary = y_binary)
+logistic_model <- glm(y_binary ~ x, data = data, family = binomial)
+summary(logistic_model)
+predicted_prob <- predict(logistic_model, type = "response")
+predicted_class <- ifelse(predicted_prob > 0.5, 1, 0)
+table(Predicted = predicted_class, Actual = y_binary)
+library(ggplot2)
+ggplot(data, aes(x = x, y = y_binary)) +
+geom_point() +
+stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE) +
+labs(title = "Logistic Regression: Binary Outcome by x",
+x = "x",
+y = "Binary Outcome")
   `,
   7: `
-  !pip install pandas
 import pandas as pd
 df=pd.read_csv('AdventureWorks Sales.csv')
 df
@@ -76,7 +75,7 @@ df.groupby('Channel').count().plot(kind='pie', y='SalesOrderLineKey', autopct='%
 figsize=(6,6))
   `,
   8: `
-  import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 file_path = "AdventureWorks Sales.csv"
 sales = pd.read_csv(file_path)
